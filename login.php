675c2df579c4dc899316876f4604982fe227e37f
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-13 20:46:50 +0300

    complete change in design

--- login.php
+++ login.php
@@ -1,145 +1,60 @@
 <?php
-# Initialize session
+
+include 'config.php';
 session_start();
 
-# Check if user is already logged in, If yes then redirect him to index page
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == TRUE) {
-  echo "<script>" . "window.location.href='./'" . "</script>";
-  exit;
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+   $select = mysqli_query($conn, "SELECT * FROM `user_info` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['user_id'] = $row['id'];
+      header('location:index.php');
+   }else{
+      $message[] = 'incorrect password or email!';
+   }
+
 }
 
-# Include connection
-require_once "./connection.php";
-
-# Define variables and initialize with empty values
-$user_login_err = $user_password_err = $login_err = "";
-$user_login = $user_password = "";
-
-# Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty(trim($_POST["user_login"]))) {
-    $user_login_err = "Please enter your username or an email id.";
-  } else {
-    $user_login = trim($_POST["user_login"]);
-  }
-
-  if (empty(trim($_POST["user_password"]))) {
-    $user_password_err = "Please enter your password.";
-  } else {
-    $user_password = trim($_POST["user_password"]);
-  }
-
-  # Validate credentials 
-  if (empty($user_login_err) && empty($user_password_err)) {
-    # Prepare a select statement
-    $sql = "SELECT id, username, password FROM users WHERE username = ? OR email = ?";
-
-    if ($stmt = mysqli_prepare($link, $sql)) {
-      # Bind variables to the statement as parameters
-      mysqli_stmt_bind_param($stmt, "ss", $param_user_login, $param_user_login);
-
-      # Set parameters
-      $param_user_login = $user_login;
-
-      # Execute the statement
-      if (mysqli_stmt_execute($stmt)) {
-        # Store result
-        mysqli_stmt_store_result($stmt);
-
-        # Check if user exists, If yes then verify password
-        if (mysqli_stmt_num_rows($stmt) == 1) {
-          # Bind values in result to variables
-          mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-
-          if (mysqli_stmt_fetch($stmt)) {
-            # Check if password is correct
-            if (password_verify($user_password, $hashed_password)) {
-
-              # Store data in session variables
-              $_SESSION["id"] = $id;
-              $_SESSION["username"] = $username;
-              $_SESSION["loggedin"] = TRUE;
-
-              # Redirect user to index page
-              echo "<script>" . "window.location.href='./'" . "</script>";
-              exit;
-            } else {
-              # If password is incorrect show an error message
-              $login_err = "The email or password you entered is incorrect.";
-            }
-          }
-        } else {
-          # If user doesn't exists show an error message
-          $login_err = "Invalid username or password.";
-        }
-      } else {
-        echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
-        echo "<script>" . "window.location.href='./login.php'" . "</script>";
-        exit;
-      }
-
-      # Close statement
-      mysqli_stmt_close($stmt);
-    }
-  }
-
-  # Close connection
-  mysqli_close($link);
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User login system</title>
-  <link rel="stylesheet" href="./css/style.css">
-  <script defer src="./js/script.js"></script>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="./style.css">
+
 </head>
-
 <body>
-  <div class="container">
-    <div>
-      <div class="col-lg-5">
-        <?php
-        if (!empty($login_err)) {
-          echo "<div class='alert alert-danger'>" . $login_err . "</div>";
-        }
-        ?>
-    
-        <div>
-        <img class="logo" src="img/logo.svg" alt="logo">
-          <h1>Log In</h1>
-          <p>Please login to continue</p>
-          <!-- form starts here -->
-          <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" novalidate>
-            <div>
-              <label for="user_login" class="form-label">Email or username</label>
-              <input type="text" class="form-control" name="user_login" id="user_login" value="<?= $user_login; ?>">
-              <small class="text-danger"><?= $user_login_err; ?></small>
-            </div>
-            <div class="mb-2">
-              <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" name="user_password" id="password">
-              <small class="text-danger"><?= $user_password_err; ?></small>
-            </div>
-            <div>
-              <input type="checkbox" class="form-check-input" id="togglePassword">
-              <label for="togglePassword" class="form-check-label">Show Password</label>
-            </div>
-            <div class="mb-3">
-              <input type="submit" class="btn btn-primary form-control" name="submit" value="Log In">
-            </div>
-            <p>Don't have an account ? <a href="./register.php">Sign Up</a></p>
-          </form>
-          <!-- form ends here -->
-        </div>
-      </div>
-    </div>
-  </div>
-</body>
 
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
+   }
+}
+?>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <input type="email" name="email" required placeholder="enter email" class="box">
+      <input type="password" name="password" required placeholder="enter password" class="box">
+      <input type="submit" name="submit" class="btn" value="login now">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+   </form>
+
+</div>
+
+</body>
 </html>
