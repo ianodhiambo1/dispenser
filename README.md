8e26bd8306420fccbe66a27fc23f933dffaa1026
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-13 10:52:59 +0300

    login page designing

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a Drug dispensary project by Allan Kamau and Ian Odhiambo
+Added Requirements
+Added Use Case Diagram
+App design in process
+Adding CSS files and PHP class diagrams
+
--- connection.php
+++ connection.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mydb";
+define("DB_SERVER", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "mydb");
 
-// Create a connection
-$connection = mysqli_connect($servername, $username, $password, $dbname);
+# Connection
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check the connection
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
+# Check connection
+if (!$link) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 ?>
