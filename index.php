69d013211b8d3b8d9086663e2ad7c0f0a667a541
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-18 10:46:10 +0300

    changing structure

--- index.php
+++ index.php
@@ -21,12 +21,15 @@ if(isset($_POST['add_to_cart'])){
    $product_image = $_POST['product_image'];
    $product_quantity = $_POST['product_quantity'];
 
-   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` 
+   WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
    if(mysqli_num_rows($select_cart) > 0){
       $message[] = 'product already added to cart!';
    }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image, quantity) VALUES('$user_id', '$product_name', '$product_price', '$product_image', '$product_quantity')") or die('query failed');
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image, quantity) 
+      VALUES('$user_id', '$product_name', '$product_price', 
+      '$product_image', '$product_quantity')") or die('query failed');
       $message[] = 'product added to cart!';
    }
 
@@ -35,18 +38,21 @@ if(isset($_POST['add_to_cart'])){
 if(isset($_POST['update_cart'])){
    $update_quantity = $_POST['cart_quantity'];
    $update_id = $_POST['cart_id'];
-   mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
+   mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' 
+   WHERE id = '$update_id'") or die('query failed');
    $message[] = 'cart quantity updated successfully!';
 }
 
 if(isset($_GET['remove'])){
    $remove_id = $_GET['remove'];
-   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
+   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or 
+   die('query failed');
    header('location:index.php');
 }
   
 if(isset($_GET['delete_all'])){
-   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or 
+   die('query failed');
    header('location:index.php');
 }
 
@@ -84,13 +90,17 @@ if(isset($message)){
 		<label class="logo">Store Logo</label>
 		<ul>
       <?php
-      $select_user = mysqli_query($conn, "SELECT * FROM `user_info` WHERE id = '$user_id'") or die('query failed');
+      $select_user = mysqli_query($conn, "SELECT * FROM `user_info` 
+      WHERE id = '$user_id'") or die('query failed');
       if(mysqli_num_rows($select_user) > 0){
          $fetch_user = mysqli_fetch_assoc($select_user);
       };
    ?>
-			<li><a href="index.php?logout=<?php echo $user_id; ?>" onclick="return confirm('are your sure you want to logout?');" class="logout">logout</a></li>
-         <img src="images/userLogo.png" alt="UserLogo" width="35px" style="padding-top: 10px;" >
+			<li><a href="index.php?logout=<?php echo $user_id; ?>" 
+         onclick="return confirm('are your sure you want to logout?');" 
+         class="logout">logout</a></li>
+         <img src="images/userLogo.png" alt="UserLogo" width="35px" 
+         style="padding-top: 10px;" >
 			<li><span><?php echo $fetch_user['name']; ?></span></li>
 		</ul>
 	</nav>
@@ -105,7 +115,8 @@ if(isset($message)){
    <div class="box-container">
 
    <?php
-      $select_product = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+      $select_product = mysqli_query($conn, "SELECT * FROM `products`") or 
+      die('query failed');
       if(mysqli_num_rows($select_product) > 0){
          while($fetch_product = mysqli_fetch_assoc($select_product)){
    ?>
@@ -114,10 +125,14 @@ if(isset($message)){
          <div class="name"><?php echo $fetch_product['name']; ?></div>
          <div class="price">$<?php echo $fetch_product['price']; ?>/-</div>
          <input type="number" min="1" name="product_quantity" value="1">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+         <input type="hidden" name="product_image" 
+         value="<?php echo $fetch_product['image']; ?>">
+         <input type="hidden" name="product_name" 
+         value="<?php echo $fetch_product['name']; ?>">
+         <input type="hidden" name="product_price" 
+         value="<?php echo $fetch_product['price']; ?>">
+         <input type="submit" value="add to cart" 
+         name="add_to_cart" class="btn">
       </form>
    <?php
       };
@@ -143,42 +158,55 @@ if(isset($message)){
       </thead>
       <tbody>
       <?php
-         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` 
+         WHERE user_id = '$user_id'") or die('query failed');
          $grand_total = 0;
          if(mysqli_num_rows($cart_query) > 0){
             while($fetch_cart = mysqli_fetch_assoc($cart_query)){
       ?>
          <tr>
-            <td><img src="images/<?php echo $fetch_cart['image']; ?>" height="100" alt=""></td>
+            <td><img src="images/<?php echo $fetch_cart['image']; ?>" 
+            height="100" alt=""></td>
             <td><?php echo $fetch_cart['name']; ?></td>
             <td>$<?php echo $fetch_cart['price']; ?>/-</td>
             <td>
                <form action="" method="post">
-                  <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
-                  <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
-                  <input type="submit" name="update_cart" value="update" class="option-btn">
+                  <input type="hidden" name="cart_id" 
+                  value="<?php echo $fetch_cart['id']; ?>">
+                  <input type="number" min="1" name="cart_quantity" 
+                  value="<?php echo $fetch_cart['quantity']; ?>">
+                  <input type="submit" name="update_cart" value="update" 
+                  class="option-btn">
                </form>
             </td>
-            <td>$<?php echo $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?>/-</td>
-            <td><a href="index.php?remove=<?php echo $fetch_cart['id']; ?>" class="delete-btn" onclick="return confirm('remove item from cart?');">remove</a></td>
+            <td>$<?php echo $sub_total = ($fetch_cart['price'] * 
+            $fetch_cart['quantity']); ?>/-</td>
+            <td><a href="index.php?remove=<?php echo $fetch_cart['id']; ?>" 
+            class="delete-btn" onclick="return confirm('remove item from cart?');"
+            >remove</a></td>
          </tr>
       <?php
          $grand_total += $sub_total;
             }
          }else{
-            echo '<tr><td style="padding:20px; text-transform:capitalize;" colspan="6">no item added</td></tr>';
+            echo '<tr><td style="padding:20px; 
+            text-transform:capitalize;" colspan="6">no item added</td></tr>';
          }
       ?>
       <tr class="table-bottom">
          <td colspan="4">grand total :</td>
          <td>$<?php echo $grand_total; ?>/-</td>
-         <td><a href="index.php?delete_all" onclick="return confirm('delete all from cart?');" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">delete all</a></td>
+         <td><a href="index.php?delete_all" 
+         onclick="return confirm('delete all from cart?');" 
+         class="delete-btn <?php echo ($grand_total > 1)?'':
+         'disabled'; ?>">delete all</a></td>
       </tr>
    </tbody>
    </table>
 
    <div class="cart-btn">  
-      <a href="#" class="btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">proceed to checkout</a>
+      <a href="#" class="btn <?php echo ($grand_total > 1)?'':
+      'disabled'; ?>">proceed to checkout</a>
    </div>
 
 </div>
