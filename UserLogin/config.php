51e4f4704b49179286030b6331c35c1fd9533e07
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-06-20 22:20:54 +0300

    redesign

--- UserLogin/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root','','mydb') or die('connection failed');
-
-?>
--- index.php
+++ index.php
@@ -68,8 +68,6 @@ if(isset($_GET['delete_all'])){
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="./style.css">
-   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
 </head>
 <body>
    
@@ -82,136 +80,24 @@ if(isset($message)){
 ?>
 
 <div class="container">
-<nav>
-		<input type="checkbox" id="check">
-		<label for="check" class="checkbtn">
-			<i class="fas fa-bars"></i>
-		</label>
-		<label class="logo">Store Logo</label>
-		<ul>
-      <?php
+
+<nav class="nav">
+   <div class="contain">
+   <img src="images/logo.svg" alt="Logo" class="siteLogo">
+   <?php
       $select_user = mysqli_query($conn, "SELECT * FROM `user_info` 
       WHERE id = '$user_id'") or die('query failed');
       if(mysqli_num_rows($select_user) > 0){
          $fetch_user = mysqli_fetch_assoc($select_user);
       };
    ?>
-			<li><a href="index.php?logout=<?php echo $user_id; ?>" 
-         onclick="return confirm('are your sure you want to logout?');" 
-         class="logout">logout</a></li>
-         <img src="images/userLogo.png" alt="UserLogo" width="35px" 
-         style="padding-top: 10px;" >
-			<li><span><?php echo $fetch_user['name']; ?></span></li>
-		</ul>
-	</nav>
-
-
-
-
-<div class="products">
-
-   <h1 class="heading">latest products</h1>
-
-   <div class="box-container">
-
-   <?php
-      $select_product = mysqli_query($conn, "SELECT * FROM `products`") or 
-      die('query failed');
-      if(mysqli_num_rows($select_product) > 0){
-         while($fetch_product = mysqli_fetch_assoc($select_product)){
-   ?>
-      <form method="post" class="box" action="">
-         <img  src="images/<?php echo $fetch_product['image']; ?>" alt="">
-         <div class="name"><?php echo $fetch_product['name']; ?></div>
-         <div class="price">$<?php echo $fetch_product['price']; ?>/-</div>
-         <input type="number" min="1" name="product_quantity" value="1">
-         <input type="hidden" name="product_image" 
-         value="<?php echo $fetch_product['image']; ?>">
-         <input type="hidden" name="product_name" 
-         value="<?php echo $fetch_product['name']; ?>">
-         <input type="hidden" name="product_price" 
-         value="<?php echo $fetch_product['price']; ?>">
-         <input type="submit" value="add to cart" 
-         name="add_to_cart" class="btn">
-      </form>
-   <?php
-      };
-   };
-   ?>
-
+   <div class="userInfo">
+      <a href="index.php?logout=<?php echo $user_id; ?>" 
+         onclick="return confirm('are your sure you want to logout?');" class="logOut">Log out</a>
+      <span><?php echo $fetch_user['name']; ?></span>
    </div>
-
-</div>
-
-<div class="shopping-cart">
-
-   <h1 class="heading">shopping cart</h1>
-
-   <table>
-      <thead>
-         <th>image</th>
-         <th>name</th>
-         <th>price</th>
-         <th>quantity</th>
-         <th>total price</th>
-         <th>action</th>
-      </thead>
-      <tbody>
-      <?php
-         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` 
-         WHERE user_id = '$user_id'") or die('query failed');
-         $grand_total = 0;
-         if(mysqli_num_rows($cart_query) > 0){
-            while($fetch_cart = mysqli_fetch_assoc($cart_query)){
-      ?>
-         <tr>
-            <td><img src="images/<?php echo $fetch_cart['image']; ?>" 
-            height="100" alt=""></td>
-            <td><?php echo $fetch_cart['name']; ?></td>
-            <td>$<?php echo $fetch_cart['price']; ?>/-</td>
-            <td>
-               <form action="" method="post">
-                  <input type="hidden" name="cart_id" 
-                  value="<?php echo $fetch_cart['id']; ?>">
-                  <input type="number" min="1" name="cart_quantity" 
-                  value="<?php echo $fetch_cart['quantity']; ?>">
-                  <input type="submit" name="update_cart" value="update" 
-                  class="option-btn">
-               </form>
-            </td>
-            <td>$<?php echo $sub_total = ($fetch_cart['price'] * 
-            $fetch_cart['quantity']); ?>/-</td>
-            <td><a href="index.php?remove=<?php echo $fetch_cart['id']; ?>" 
-            class="delete-btn" onclick="return confirm('remove item from cart?');"
-            >remove</a></td>
-         </tr>
-      <?php
-         $grand_total += $sub_total;
-            }
-         }else{
-            echo '<tr><td style="padding:20px; 
-            text-transform:capitalize;" colspan="6">no item added</td></tr>';
-         }
-      ?>
-      <tr class="table-bottom">
-         <td colspan="4">grand total :</td>
-         <td>$<?php echo $grand_total; ?>/-</td>
-         <td><a href="index.php?delete_all" 
-         onclick="return confirm('delete all from cart?');" 
-         class="delete-btn <?php echo ($grand_total > 1)?'':
-         'disabled'; ?>">delete all</a></td>
-      </tr>
-   </tbody>
-   </table>
-
-   <div class="cart-btn">  
-      <a href="#" class="btn <?php echo ($grand_total > 1)?'':
-      'disabled'; ?>">proceed to checkout</a>
    </div>
-
-</div>
-
-</div>
-
+   
+</nav>
 </body>
 </html>
